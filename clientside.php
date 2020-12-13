7d4b8d8f59a8d244e44c5992fe478eacdef82abe
iankiragu <iankiragu63@gmail.com>
2020-12-13 14:12:30 +0300

    corrected errors

--- clientside.php
+++ clientside.php
@@ -3,15 +3,9 @@
 <head>
 <meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="icon" type="image/png" href="ico.png">
-	<link rel="stylesheet" type="text/css" href="vendor/bootstrap/css/bootstrap.min.css">
-	<link rel="stylesheet" type="text/css" href="fonts/font-awesome-4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" type="text/css" href="fonts/font-awesome-4.7.0/css/font-awesome.min.css">
 	<link rel="stylesheet" type="text/css" href="fonts/Linearicons-Free-v1.0.0/icon-font.min.css">
-	<link rel="stylesheet" type="text/css" href="vendor/animate/animate.css">
-	<link rel="stylesheet" type="text/css" href="vendor/css-hamburgers/hamburgers.min.css">
-	<link rel="stylesheet" type="text/css" href="vendor/animsition/css/animsition.min.css">
-	<link rel="stylesheet" type="text/css" href="vendor/select2/select2.min.css">
-	<link rel="stylesheet" type="text/css" href="vendor/daterangepicker/daterangepicker.css">
+	<link type="text/css" rel="stylesheet" href="css/style.css"/>
 	<link rel="stylesheet" type="text/css" href="css/util.css">
 	<link rel="stylesheet" type="text/css" href="css/main.css">
 </head>
@@ -41,7 +35,7 @@
             #pass wsdl file address
             $client = new nusoap_client("https://localhost/SOAP/serverside.php?wsdl");
 
-            $student_id= $_POST['student_id'];
+            $student_id= isset($_POST['student_id']);
             $client->call('fetchstudent', array("student_id" => $student_id));
             $name = $client->response;
             echo 'Response:<br>';
